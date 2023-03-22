73e0b44d15f1f9eeb1466a1ad966e428ea0d15aa
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-03-21 18:18:44 -0600

    Add testing to call on ./lib/deck.rb methods.

---
+++ spec/deck_spec.rb
@@ -0,0 +1,128 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+
+
+RSpec.describe Deck do
+
+  it "exists" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    
+    expect(deck).to be_an_instance_of(Deck)
+    
+  end
+
+  it "shows deck" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck.cards).to eq(cards)
+  end
+
+  it "finds rank of card" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck.rank_of_card_at(0)).to eq(12)
+    expect(deck.rank_of_card_at(2)).to eq(14)
+
+  end
+
+  it "finds high-ranking cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck.high_ranking_cards).to eq([card1, card3])
+  end
+
+  it "calculates high-ranking cards percentage" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.percent_high_ranking).to eq(66.67)
+  end
+
+  it "removes the top card" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    deck.remove_card
+    expect(deck.cards).to eq([card2, card3])
+  end
+
+  it "finds new high-ranking cards after removal" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    deck.remove_card
+    expect(deck.high_ranking_cards).to eq([card3])
+  end
+
+  it "calculates new high-ranking cards percentage after removal" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    deck.remove_card 
+    expect(deck.percent_high_ranking).to eq(50)
+  end
+
+  card4 = Card.new(:club, '5', 5)
+
+  it "adds a new card to the deck" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    card4 = Card.new(:club, '5', 5)
+    deck = Deck.new(cards)
+    expect(deck.add_card(card4)).to eq([card1, card2, card3, card4])
+  end
+
+  it "finds new high-ranking cards after addition" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    card4 = Card.new(:club, '5', 5)
+    deck = Deck.new(cards)
+    deck.remove_card
+    deck.add_card(card4)
+    expect(deck.high_ranking_cards).to eq([card3])
+  end 
+
+  it "calculates new high-ranking cards percentage after addition" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    card4 = Card.new(:club, '5', 5)
+    deck = Deck.new(cards)
+    deck.remove_card
+    deck.add_card(card4)
+    expect(deck.percent_high_ranking).to eq(33.33)
+  end
+
+end
+
+
+
+
