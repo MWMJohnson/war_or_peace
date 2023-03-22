8ebcb384c6f5191909c928f970a32a051b1a9e31
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-03-21 18:14:41 -0600

    Add class Deck content.

---
+++ lib/deck.rb
@@ -0,0 +1,36 @@
+class Deck
+  attr_reader :cards
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def rank_of_card_at(i)
+     @cards[i].rank
+  end
+
+  def high_ranking_cards
+    highest_ranked_cards = @cards.select do |card|
+      card.rank >= 11
+    end
+  end
+
+
+  def percent_high_ranking
+      num_highest_ranked_cards = @cards.count do |card|
+        card.rank >= 11
+      end
+      num_of_total_cards = @cards.count
+      percent_of_highest_ranked_cards = num_highest_ranked_cards.to_f / num_of_total_cards.to_f * 100
+      percentage_rounded = (percent_of_highest_ranked_cards).round(2)
+  end
+
+  def remove_card
+      @cards.delete_at(0)
+  end
+
+
+  def add_card(card)
+      @cards << card
+  end
+
+end
